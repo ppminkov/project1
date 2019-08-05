3e42c75161a1113a13d9efb7d7aa5e67df636b1a
Petko Minkov <ppminkov@gmail.com>
2019-08-05 10:20:44 +0200

    no message

--- edit_config_file.sh
+++ edit_config_file.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
+# edit a file with sed
+# update a specific value
+# adding an empty line at the end
 
-FILE_TO_EDIT="/tmp/setenv.sh"
-#/var/lib/vco/app-server/bin/setenv.sh
-
+FILE_TO_EDIT='/var/lib/vco/app-server/bin/setenv.sh'
+BACKUP_DIR="/tmp/setenv`date +"%Y-%m-%d-%m-%s"`"
 GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 
 echo "=== Back up the initial configuration"
-if ! cp $FILE_TO_EDIT $FILE_TO_EDIT + "%Y-%m-%d-%m-%s"` ; then echo "Replace value failed"; exit 1; fi
+if ! cp ${FILE_TO_EDIT} $BACKUP_DIR  ; then echo "Backup file failed"; exit 1; fi
 
-# Update value
+# Update a value
 echo "=== Set a value..."
 if ! sed -i -e 's/Disable SNIExtension/Enable SNIExtension/g' $FILE_TO_EDIT ; then echo "=== ${ORANGE} Replace value failed"; exit 1; fi
 if ! sed -i -e 's/enableSNIExtension=false/enableSNIExtension=true/g' $FILE_TO_EDIT ; then echo "=== ${ORANGE}Replace value failed"; exit 1; fi
 
-# Add new lines in the config file on rows 36 and 37
-if ! sed -e '36i#Enable TSL1.1, TSL1.2' -i $FILE_TO_EDIT ; then echo "${ORANGE} Adding the new value failed"; exit 1; fi
-if ! sed -e '37iJVM_OPTS="$JVM_OPTS -Dhttps.protocols=SSLv3,TLSv1,TLSv1.1,TLSv1.2 -Djdk.tls.client.protocols=TLSv1,TLSv1.1,TLSv1.2"' -i $FILE_TO_EDIT ; then echo "${ORANGE} Adding the new value failed"; exit 1; fi
+# Add new lines in the config file on specific rows with new line at the end
+echo "=== Adding new lines..."
+if ! sed -e '36i#Enable TSL1.1, TSL1.2' -i $FILE_TO_EDIT ; then echo "${ORANGE} Adding the new line failed"; exit 1; fi
+if ! sed -e '37iJVM_OPTS="$JVM_OPTS -Dhttps.protocols=SSLv3,TLSv1,TLSv1.1,TLSv1.2 -Djdk.tls.client.protocols=TLSv1,TLSv1.1,TLSv1.2"\n' -i $FILE_TO_EDIT ; then echo "${ORANGE} Adding the new value failed"; exit 1; fi
 
 # Restart vco-server
-#if ! service vco-server restart ; then echo "Replace value failed"; exit 1; fi
+echo "=== Restarting vRO server to apply the changes..."
+if ! service vco-server restart ; then echo "${ORANGE}VCO server restart failed"; exit 1; fi
 
-echo -e "=== ${GREEN}The configuration file has been modified successfully!..."
+echo -e "=== ${GREEN}The configuration changes have been done successfully!..."
